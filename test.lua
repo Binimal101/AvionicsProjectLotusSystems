f924dd4ae85a44bc1c50bc278a479bf9422eb976
ezhang <ericzhang200@gmail.com>
2025-03-11 13:11:56 -0400

    inital commit

---
+++ test.lua
@@ -0,0 +1,20 @@
+def init():
+    if(arming:is_armed()) then
+       arming:disarm() 
+    end
+    -- get ground data
+    initial_position = ahrs:get_position()
+    initial_home = ahrs:get_home()
+    initial_alt = ahrs:get_hagl()
+    -- reading acceleration
+    while ahrs:get_accel():z() > 0 do
+        print("(still accelerating upwards)")
+    end
+    vehicle:set_mode(18) --throw mode
+    
+    while !ahrs:prearm_healthy() do
+        print("Prearm checks not yet ready.")
+    end
+    arming:arm()
+    send_gpio()
+end
